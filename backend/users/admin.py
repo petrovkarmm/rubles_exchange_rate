7daa8013aa29cd806b8b0bc1aef98fc955308d3f
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 02:26:11 +0500

    creating admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import TelegramUser, TelegramUserActivity
+
+
+@admin.register(TelegramUser)
+class PostAdmin(admin.ModelAdmin):
+    # Отображение полей
+    list_display = ['tg_user_id', 'tg_user_first_name', 'tg_user_last_name', 'tg_user_username']
+    # Лист фильтрация справа
+    list_filter = ['tg_user_registration_time']
+    # Строка поиска
+    search_fields = ['tg_user_username']
+    # Сортировка по стобцам по умолчанию
+    ordering = ['tg_user_registration_time']
+
+
+@admin.register(TelegramUserActivity)
+class PostAdmin(admin.ModelAdmin):
+    # Отображение полей
+    list_display = ['tg_user_id', 'tg_user_username', 'activity_time']
+    # Лист фильтрация справа
+    list_filter = ['activity_time']
+    # Строка поиска
+    search_fields = ['tg_user_username']
+    # Сортировка по стобцам по умолчанию
+    ordering = ['activity_time']
