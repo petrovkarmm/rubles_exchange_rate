a94e13f7383a144824580bd7314e51b977cb43c5
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 02:24:39 +0500

    add fetchs

--- app/routers/registration_router/registration_router.py
+++ app/routers/registration_router/registration_router.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.filters import Command, StateFilter
 from aiogram.types import Message
 
+from app.data_fetcher import django_user_registration, django_user_check_registration
 from app.routers.main_menu_router.keyboards.main_menu_buttons import main_menu_kb
 from app.routers.registration_router.keyboards.path_to_registration_button import start_register_kb
 from app.routers.registration_router.keyboards.registration_buttons import register_kb
@@ -14,14 +15,27 @@ router = Router()
 
 @router.message(StateFilter(None), Command("start"))
 async def cmd_start(message: Message, state: FSMContext):
-    # TODO Добавить запрос к БД о наличии проверки пользователя в БД и переводить в end registration, если есть
-    await message.answer(
-        "Для дальнейшей работы с ботом вам необходимо пройти регистрацию."
-        "\nНажав на кнопку Зарегистрироваться вы принимаете условия на обработку данных, а именно: "
-        "\nИмя, фамилия, а так же имя пользователя и его ID.",
-        reply_markup=register_kb()
-    )
-    await state.set_state(Register.start_registration)
+
+    data = {
+        "tg_user_id": message.from_user.id,
+    }
+
+    response = await django_user_check_registration(data=data)
+
+    if response.get('result'):
+        await message.answer(
+            "Для дальнейшей работы с ботом вам необходимо пройти регистрацию."
+            "\nНажав на кнопку Зарегистрироваться вы принимаете условия на обработку данных, а именно: "
+            "\nИмя, фамилия, а так же имя пользователя и его ID.",
+            reply_markup=register_kb()
+        )
+        await state.set_state(Register.start_registration)
+    else:
+        await message.answer(
+            'Вы уже были зарегистрированы в системе. Произвожу авторизацию.',
+            reply_markup=main_menu_kb()
+        )
+        await state.set_state(Register.end_registration)
 
 
 @router.message(StateFilter(None), F.text.lower() == 'начало регистрации')
@@ -37,25 +51,60 @@ async def cmd_start(message: Message, state: FSMContext):
 
 @router.message(Register.start_registration, F.text.lower() == 'зарегистрироваться')
 async def end_registration_handler(message: Message, state: FSMContext):
+
+    data = {
+        "tg_user_id": message.from_user.id,
+        "tg_user_first_name": message.from_user.first_name,
+        "tg_user_last_name": message.from_user.last_name,
+        "tg_user_username": message.from_user.username,
+        "tg_user_language": message.from_user.language_code,
+    }
+
+    response = await django_user_registration(data)
+
+    if response.get('result'):
         await message.answer(
             "Регистрация прошла успешно.",
             reply_markup=main_menu_kb()
         )
+
+        await state.set_state(Register.end_registration)
+
+    else:
+        await message.answer(
+            "Вы уже были зарегистрированы в системе. Произвожу авторизацию.",
+            reply_markup=main_menu_kb()
+        )
+
         await state.set_state(Register.end_registration)
 
 
 @router.message(Register.start_registration, F.text.lower() == 'отказ от регистрации')
 async def end_registration_handler(message: Message, state: FSMContext):
-        await message.answer(
-            "Отказ от регистрации. Если вы передумаете, нажмите кнопку ниже.",
-            reply_markup=start_register_kb()
-        )
-        await state.set_state(None)
+    await message.answer(
+        "Отказ от регистрации. Если вы передумаете, нажмите кнопку ниже.",
+        reply_markup=start_register_kb()
+    )
+    await state.set_state(None)
 
 
 @router.message(StateFilter(None), F.text)
-async def registration_error_handler(message: Message):
-    await message.answer(
-        "Регистрация не пройдена. Для начала прохождения регистрации нажмите кнопку ниже.",
-        reply_markup=start_register_kb()
-    )
+async def registration_error_handler(message: Message, state: FSMContext):
+    data = {
+        "tg_user_id": message.from_user.id,
+    }
+
+    response = await django_user_check_registration(data)
+
+    if response.get('result'):
+        await message.answer(
+            "Регистрация не пройдена. Для начала прохождения регистрации нажмите кнопку ниже.",
+            reply_markup=start_register_kb()
+        )
+    else:
+        await message.answer(
+            "Произвожу повторную авторизацию. Пожалуйста, повторите команду.",
+            reply_markup=main_menu_kb()
+        )
+
+        await state.set_state(Register.end_registration)
