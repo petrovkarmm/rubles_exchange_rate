d1b55d3b19ebb912787cbadfc74a9696066e8a96
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 02:22:08 +0500

    update bot adding 2 routers/cron and cancel_choice

--- app/bot.py
+++ app/bot.py
@@ -1,23 +1,50 @@
 import os
 import asyncio
 
+from aiogram.types import Message
+
+import apsched
+from datetime import datetime
+
 from app.routers.main_menu_router import main_menu_router
+from app.routers.main_menu_router.keyboards.main_menu_buttons import main_menu_kb
 from app.routers.registration_router import registration_router
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from dotenv import load_dotenv, find_dotenv
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
+
+from app.routers.registration_router.utils.states import Register
 
 load_dotenv(find_dotenv())
+
+
+async def cancel_choice(message: Message):
+    await message.answer(
+        text="Возвращаемся в главное меню.",
+        reply_markup=main_menu_kb()
+    )
 
 
 async def main():
     bot = Bot(token=os.environ.get('AIOGRAM_API_KEY'))
     dp = Dispatcher()
 
+    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+    scheduler.add_job(apsched.send_currency_for_subs, trigger='cron',
+                      hour=12,
+                      minute=0, kwargs={'bot': bot})
+
+    # Тест работы отправки сообщения по подписке.
+    # scheduler.add_job(apsched.send_currency_for_subs, trigger='interval', seconds=15, kwargs={'bot': bot})
+    scheduler.start()
+
     dp.include_router(registration_router.router)
 
     dp.include_router(main_menu_router.router)
+
+    dp.message.register(cancel_choice, Register.end_registration, F.text.lower() == '❌ назад.')
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
