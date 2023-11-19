2fc832ce75c9eb5703deb295c24b068c708b60dd
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 02:24:14 +0500

    update backend models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,16 +1,39 @@
-import uuid
-
-from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
-class TelegramProfile(models.Model):
-    # TODO: add lengths on fields(prob change fields size)
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             verbose_name='Django Default User')
-    tg_user_id = models.IntegerField(verbose_name='User telegram ID')
-    tg_user_language = models.CharField(max_length=30, verbose_name='User telegram language')
+class TelegramUser(models.Model):
+    # TODO add length on model
+    tg_user_id = models.IntegerField(primary_key=True)
+    tg_user_first_name = models.CharField(max_length=30, blank=True, null=True)
+    tg_user_last_name = models.CharField(max_length=30, blank=True, null=True)
+    tg_user_username = models.CharField(max_length=30, blank=True, null=True)
+    tg_user_language = models.CharField(max_length=5, blank=True, null=True)
+    tg_user_registration_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    have_a_subscription = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.tg_user_id, self.user.username}'
+        return f'{self.tg_user_id, self.tg_user_username, self.have_a_subscription}'
+
+
+class TelegramUserActivity(models.Model):
+    tg_user_id = models.IntegerField()
+    tg_user_username = models.CharField(max_length=30, blank=True, null=True)
+    tg_handler_name = models.CharField(max_length=60, verbose_name='Название активности в telegram')
+    activity_time = models.DateTimeField(auto_now_add=True, verbose_name='Время актиновсти в telegram')
+
+    def __str__(self):
+        return f'{self.tg_user_username}, {self.activity_time}'
+
+
+# Функция-обработчик для сигнала pre_save
+# Если количество записей пользователя больше 15, удаляет самую старую по дате и добавляет новую.
+@receiver(pre_save, sender=TelegramUserActivity)
+def update_activity_time(sender, instance, **kwargs):
+    if TelegramUserActivity.objects.filter(tg_user_id=instance.tg_user_id).count() >= 15:
+        oldest_activity = (TelegramUserActivity.objects.filter(tg_user_id=instance.tg_user_id)
+                           .order_by('activity_time')
+                           .first())
+        oldest_activity.delete()
