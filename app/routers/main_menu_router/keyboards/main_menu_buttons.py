7e7b61a34fab949dc0cdf70992977dfa87c6d12a
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 02:23:13 +0500

    updating main menu kb/ add without_subscribe kb / with_subscribe kb

--- app/routers/main_menu_router/keyboards/main_menu_buttons.py
+++ app/routers/main_menu_router/keyboards/main_menu_buttons.py
@@ -4,10 +4,36 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def main_menu_kb():
     keyboard_builder = ReplyKeyboardBuilder()
 
-    keyboard_builder.button(text='🚀 Старт')
     keyboard_builder.button(text='🌐 Информация о пользователе.')
     keyboard_builder.button(text='💰 Текущий курс рубля.')
-    keyboard_builder.button(text='⏳ Помощь')
+    keyboard_builder.button(text='💌 Подписка.')
+    keyboard_builder.button(text='📖 Моя история запросов.')
+
+    keyboard_builder.adjust(2)
+    return keyboard_builder.as_markup(
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+
+def with_subscribe():
+    keyboard_builder = ReplyKeyboardBuilder()
+
+    keyboard_builder.button(text='⚠️ Отписаться.')
+    keyboard_builder.button(text='❌ Назад.')
+
+    keyboard_builder.adjust(2)
+    return keyboard_builder.as_markup(
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+
+def without_subscribe():
+    keyboard_builder = ReplyKeyboardBuilder()
+
+    keyboard_builder.button(text='✅ Подписаться.')
+    keyboard_builder.button(text='❌ Назад.')
 
     keyboard_builder.adjust(2)
     return keyboard_builder.as_markup(
