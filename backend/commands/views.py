df9e77c4b3833e10ae147b8d3239cd5c74c80fd9
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 20:43:23 +0500

    view for command-text url

--- backend/commands/views.py
+++ backend/commands/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from commands.models import AiogramCommands
+
+
+class GetTextForCommand(APIView):
+    def get(self, request: Request):
+        command = request.data.get('command')
+
+        try:
+            command_obj = AiogramCommands.objects.get(command=command)
+            result = {
+                'text': command_obj.text
+            }
+
+            return Response(result, status=status.HTTP_200_OK)
+
+        except:
+            result = {
+                'text': None
+            }
+
+            return Response(result, status=status.HTTP_200_OK)
