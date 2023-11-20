24c11e9cd12fdbe13f3c71731690eeb0ee92e715
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-20 20:42:09 +0500

    AiogramCommands model

--- backend/commands/models.py
+++ backend/commands/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class AiogramCommands(models.Model):
+    command = models.CharField(max_length=30)
+    text = models.TextField(max_length=150)
